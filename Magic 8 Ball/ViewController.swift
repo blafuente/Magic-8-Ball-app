ab01abf7faf43c0adc80eb74377306c9c218dce9
Brian Lafuente <loulafuente@MacBook-Pro.attlocal.net>
2019-07-25 14:11:55 -0400

    added comments

--- Magic 8 Ball/ViewController.swift
+++ Magic 8 Ball/ViewController.swift
@@ -10,27 +10,32 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    // Array of ball images/answers
     let ballArray = ["ball1", "ball2", "ball3", "ball4", "ball5"]
-    
+    // set variable to zero
     var randomBallNumber = 0
-    
+    //Linking ball image to code
     @IBOutlet weak var magic8Ball: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         updateBallAnswer()
+        // onload, ball image will be random from function
     }
 
     
     @IBAction func askButtonPressed(_ sender: UIButton) {
+        // generate an answer when ask button is pressed
         updateBallAnswer()
     }
     
     override func motionEnded(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
+        // generate an answer when phone is shaken
         updateBallAnswer()
     }
     
+    // function to update the ball image/answer
     func updateBallAnswer() {
         randomBallNumber = Int.random(in: 0 ... 4)
         magic8Ball.image = UIImage(named: ballArray[randomBallNumber])
